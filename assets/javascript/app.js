0e4a1de8791088aba9cd877101b0304927dcfcae
sumirpv <sumirpv@gmail.com>
2018-02-26 18:28:59 -0800

    Modified files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,80 @@
 $(document).ready(function(){
-  alert("I am working")
 
-
-
-
-$.fn.trivia = function() {
+  jQuery.fn.triviaGame= function() {
+  //assign this to the variable obj 
   var obj = this;
-  obj.userPick = null;
-  obj.answers = {
-      correct: 0,
-      incorrect: 0
-  };
-  obj.images = null;
-  obj.count = 30;
-  obj.current = 0;
+    //timer function
+    obj.timer = function() {
+        obj.count--;
+        if (obj.count <= 0) {
+            setTimeout(function() {
+                obj.nextQuestion();
+            });
+  
+        } else {
+          //display time
+            $("#timer").html("Time remaining: " + obj.count + " secs");
+        }
+    };
+  //creating an array of objects
   obj.questions = [{
     question: "Name the school that Harry Potter attended?",
-    choices: ["Hogwarts", "Chennai", "Rasipuram", "Miracat"],
-    images: ["../images/Rajah.gif"],
+    options: ["Hogwarts", "Chennai", "Rasipuram", "Miracat"],
     correct: 0
 }, {
     question: "Which country is home to the kangaroo?",
-    choices: ["India", "America","Australia","China"],
+    options: ["India", "America","Australia","China"],
     correct: 2
 
 }, {
     question: "Which river flows through London?",
-    choices: ["Rocky Mountain", "The Thames", "The Ganges", "The Nile"],
+    options: ["Rocky Mountain", "The Thames", "The Ganges", "The Nile"],
     correct: 1
 
 }, {
     question: "How many colours are in a rainbow?",
-    choices: ["4 Colors", "6 Colors", "8 Colors", "7 Colors"],
+    options: ["4 Colors", "6 Colors", "8 Colors", "7 Colors"],
     correct: 3
 
 }, {
     question: "Which Italian city is famous for its leaning tower?",
-    choices: ["Pisa", "Eiffel Tower", "Skylon Tower", "Space Needle"],
+    options: ["Pisa", "Eiffel Tower", "Skylon Tower", "Space Needle"],
     correct: 0
 
 }, {
     question: "What food do Giant Pandas normally eat?",
-    choices: ["PineCone", "Turmuric", "Bamboo", "Cucumber"],
+    options: ["PineCone", "Turmuric", "Bamboo", "Cucumber"],
     correct: 2
 
 }, {
     question: "What is the name of the fairy in Peter Pan?",
-    choices: ["Sugar Plum Fairy", "Tooth Fairy", "Tinkerbell", "White Fairy"],
+    options: ["Sugar Plum Fairy", "Tooth Fairy", "Tinkerbell", "White Fairy"],
     correct: 2
 
 }, {
     question: "How many days are there in a fortnight?",
-    choices: ["12 Days", "21 Days", "9 Days", "14 Days"],
+    options: ["12 Days", "21 Days", "9 Days", "14 Days"],
     correct: 3
 }];
-  obj.ask = function() {
+console.log(obj.questions);
+console.log(obj.questions[3]);
+console.log(obj.questions[3].correct);
+obj.userPick = null;
+//create an answers object 
+obj.answers = {
+    correct: 0,
+    incorrect: 0
+};
+obj.count = 30;
+obj.current = 0;
+//displaying the question and anwer chocie
+  obj.display = function() {
+
       if (obj.questions[obj.current]) {
-          $("#timer").html("Time remaining: " + "00:" + obj.count + " secs");
+        //display timer
+          $("#timer").html("Time remaining: "  + obj.count + " secs");
           $("#question_div").html(obj.questions[obj.current].question);
-          var choicesArr = obj.questions[obj.current].choices;
+          var choicesArr = obj.questions[obj.current]. options;
           var buttonsArr = [];
 
           for (var i = 0; i < choicesArr.length; i++) {
@@ -67,55 +83,58 @@ $.fn.trivia = function() {
               button.attr('data-id', i);
               $('#choices_div').append(button);
           }
+          //setting the interval
           window.triviaCounter = setInterval(obj.timer, 1000);
+          //display the answers in a button
       } else {
+        // display the correct answer number
+          $('body').append($('<div>'+'</div>', {
+            text: 'Results are : ' ,
+            class: 'result'
+        }));
+       // display the correct answer number
           $('body').append($('<div>'+'</div>', {
             text: 'Correct Answers: ' + (
                 obj.answers.correct  ),
             class: 'result'
         }));
+        //display the incorrect answer in the div
         $('body').append($('<div>'+'</div>', {
           text: 'Incorrect Answers: ' + (
             obj.answers.incorrect ),
           class: 'result'
       }));
+      // this one is for un snsweered questions
       $('body').append($('<div>'+'</div>', {
         text: 'Unanswered: ' + (
             obj.questions.length - (obj.answers.correct + obj.answers.incorrect)),
         class: 'result'
     }));
-         
+         // add all the results next to start button
           $('#start_button').text('Restart').appendTo('body').show();
       }
   };
-  obj.timer = function() {
-      obj.count--;
-      if (obj.count <= 0) {
-          setTimeout(function() {
-              obj.nextQ();
-          });
 
-      } else {
-          $("#timer").html("Time remaining: " + "00:" + obj.count + " secs");
-      }
-  };
-  obj.nextQ = function() {
+  //next question function to display the questions
+  obj.nextQuestion = function() {
       obj.current++;
       clearInterval(window.triviaCounter);
       obj.count = 30;
       $('#timer').html("");
       setTimeout(function() {
-          obj.cleanUp();
-          obj.ask();
+          obj.update();
+          obj.display();
       }, 1000)
   };
-  obj.cleanUp = function() {
+  // creating a update function
+  obj.update = function() {
       $('div[id]').each(function(item) {
           $(this).html('');
       });
       $('.correct').html('Correct answers: ' + obj.answers.correct);
       $('.incorrect').html('Incorrect answers: ' + obj.answers.incorrect);
   };
+  //creating a function to check if the answer is correct or not using itenary
   obj.answer = function(correct) {
       var string = correct ? 'correct' : 'incorrect';
       obj.answers[string]++;
@@ -123,23 +142,24 @@ $.fn.trivia = function() {
   };
   return obj;
 };
+//creating a variable
 var Trivia;
-
+// if the button was clicked to do this action
 $("#start_button").click(function() {
   $(this).hide();
   $('.result').remove();
   $('div').html('');
-  Trivia = new $(window).trivia();
-  Trivia.ask();
+  Trivia = new $(window).triviaGame();
+  Trivia.display();
 });
-
+//creating a function for clicking the choices
 $('#choices_div').on('click', 'button', function(e) {
   console.log("I am clicked");
   var userPick = $(this).data("id"),
-      obj = Trivia || $(window).trivia(),
+      obj = Trivia || $(window).triviaGame(),
       index = obj.questions[obj.current].correct,
-      correct = obj.questions[obj.current].choices[index];
-
+      correct = obj.questions[obj.current]. options[index];
+//checking if the user picked the correct answer or not
   if (userPick !== index) {
       $('#choices_div').text("Wrong Answer! The correct answer was: " + correct);
       obj.answer(false);
@@ -147,7 +167,7 @@ $('#choices_div').on('click', 'button', function(e) {
       $('#choices_div').text("Correct!!! The correct answer was: " + correct);
       obj.answer(true);
   }
-  obj.nextQ();
+  obj.nextQuestion();
 });
 
 
@@ -162,283 +182,3 @@ $('#choices_div').on('click', 'button', function(e) {
 
 
 
-
-
-
-
-
-
-
-
-
-// $(document).ready(function(){
-   
-// //variable declaration
-// var questions;
-// var answers=[];
-// //object declaration
-// var trivia = {
-//    question1 : " Name the school that Harry Potter attended?",
-//    ans1 : "Hogwarts",
-//    answers1 :[ "Hogwarts", "Chennai", "Rasipuram"],
-//    question2 : "Which country is home to the kangaroo?",
-//    ans2 : "Australia",
-//    answers2 : ["Australia","India", "America"],
-//    Question3 : "How many colours are in a rainbow?",
-//    ans3 : "7",
-//    answer3 : ["4", "6", "8", "7"]
-
-// }
-// var arrofobject=[
-//     { question :"Name the school that Harry Potter attended? ",
-//         options:{
-//            a :  "Hogwarts",
-//            b : "Chennai",
-//            c : "Rasipuram"
-//                 },
-//         answer : "a"
-//      },
-//     {
-//         question : "Which country is home to the kangaroo? ",
-//             options:{
-//                 a : "Australia",
-//                 b : "India",
-//                 c :  "America"
-//                  },
-//             answer : "a"
-//                 },
-//    {
-//        question :"How many colours are in a rainbow? ",
-//             options :{
-//                 a : "4",
-//                 b : "6", 
-//                 c : "7"
-//              },
-//              answer : "c"
-//             }
-//         ];
-
-
-// /* console.log(trivia);
-// console.log(trivia.question1);
-// console.log(trivia.answers1); */
-// //console.log(arrofobject[0]);
-
-// function check(var1, var2){
-//     if ( var1 === var2){
-//         console.log(" true");
-//     }
-//     else{
-//         console.log("false");
-//     }
-
-// }
-// var checkingAnswer;
-
-// $(".btn").click(function () { 
-//     $(".btn").hide();
-
-//     $.each(arrofobject, function(index, val) {
-//         $("#question").text(val.question1);
-//         console.log(val.question);
-//         $("#answer").html("<button class='answerBtn'>"+val.options.a +"</button> <br>");
-//        // $("#answer").append("<button class='answerBtn'>"+val.options.b +"</button> <br>");
-//        // $("#answer").append("<button class='answerBtn'>"+val.options.c +"</button> <br>");
-
-
-
-//         console.log( val.options.a);
-//         console.log( val.options.b);
-//         console.log( val.options.c);
-
-        
-//     });
-
-
-
-
-//     $("#question").text(trivia.question1);
-//     var answer=trivia.answers1;
-
-
-    // var txt="";
-
-    
-    // for (var x=0 in trivia){
-    //     txt += trivia[x];
-    //     setInterval(txt, 500);
-    //     console.log(txt);
-
-
-    // }
-
-
-
-   
-    //trying to print answer1 and checking with correct answer
-    // for(var i=0; i< answer.length;i++){
-        
-    //     $("#answer").append("<button class='answerBtn'>"+answer[i] +"</button> <br>");
-    //     console.log(answer[i]);
-    //     $(".answerBtn").on("click", function(){
-
-    //       checkingAnswer=  check(trivia.ans1, answer[i]);
-            
-    //     })
-    //     console.log(" this is check" + checkingAnswer);
-
-    // }
-
-  
-//  });
-
-//  $.each(trivia, function(index, val) {
-//     console.log(val.question1);
-// });
-
-// function
-// $.each(obj, function(i,val){
-//     $(".main").append(i + " =>" + val + " <br/>") ;
-//     console.log(question);
-// });
-
-
-
-// // });
-
-// $(document).ready(function() {
-// var userPick =null ;
-
-// var correctAnswer = 0;
-
-// var incorrectAnswer = 0;
-
-// var unAnswer = 0;
-
-// var question = 0;
-
-// var images;
-
-// var count=10;
-// var current =0;
-
-// var disneyQuestion = [{
-// question: "In Aladdin, what is the name of Jasmine's pet tiger?",
-// choices: ["Rajah", "Bo", "Iago", "Jack" ],
-// //images:  ["../images/Rajah.gif"],
-// correct: 0
-// }, {
-// question:"In Peter Pan, Captain Hook had a hook on which part of his     body?",
-// choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
-// correct: 1
-
-// }, {
-// question:"In the Lion King, where does Mufasa and his family live?",
-// choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
-// correct: 3
-
-// }, {
-// question:"In Beauty and the Beast, how many eggs does Gaston eat for    breakfast?",
-// choices: ["2 Dozen", "5 Dozen", "5000", "0"],
-// correct: 1
-
-// }, {
-// question:"In Alice in Wonderland, what is the name of Alice’s kitten?",
-// choices: ["Dinah", "Sammie", "Kat", "Luna"],
-// correct: 0
-
-//  }, {
-// question:"After being on earth, where did Hercules first meet his   father Zeus?",
-// choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
-// correct: 2
-
-// }, {
-// question:"During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
-// choices: ["Yellow", "Blue", "Gold", "White"],
-// correct: 2
-
-// }, {
-// question:"In Bambi, what word does the owl use to describe falling in love?",
-// choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
-// correct: 3
-
-// }
-
-// ];
-
-// $('body').on('click', '#reset',function (){
-
-//     var userPick =null ;
-
-//     var correctAnswer = 0;
-    
-//     var incorrectAnswer = 0;
-    
-//     var unAnswer = 0;
-    
-//     var question = 0;
-    
-//     var images;
-    
-//     timer();
-//     displayTrivia();
-
-
-// });
-  
-
-// $("#start_button").click(function(){
-//   //  alert("i am pressed");
-// $(this).hide();
-// counter = setInterval(timer, 1000); 
-// displayTrivia(0);
-
-// }); 
-
-
-// function timer(){
-// count--;
-// if (count <= 0) {
-//  clearInterval(counter);
-//  return;
-// }
-
-
-//  $("#timer").html("Time remaining: " + "00:" + count + " secs");
-// }
-
-
-// function displayTrivia(questionNum) {
-// $("#question_div").html(disneyQuestion[questionNum].question);
-// question++;
-
-//   var choicesArr = disneyQuestion[questionNum].choices;
-//   var buttonsArr = [];
-
-//   for (let i = 0; i < choicesArr.length; i++) {
-//     var newdiv = $("<div id='clickdiv'>"+"</div><br>");
-//     newdiv.text(choicesArr[i]);
-//     newdiv.attr('data-id', i);
-//     $('#choices_div').append(newdiv);
-//    }
-
-//   } 
-
-//  $('#choices_div').on('click', '#clickdiv', function(e){
-//  userPick = $(this).data("id");
-//  disneyQuestion[0].correct;
-//  if(userPick != disneyQuestion[0].correct) {
-
-//  $('#choices_div').text("Wrong Answer! The correct answer is Rajah.");
-//  incorrectAnswer++;
-
-// } else if (userPick === disneyQuestion[0].correct) {
-// $('#choices_div').text("Correct!!! The pet tiger name is Rajah");
-// correctAnswer++;
-
-// }
-
-
-// });
-
-// });
